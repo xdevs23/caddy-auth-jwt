d20f9352cb02081fe6247dcd693be4df3966f0b6
Simão Gomes Viana <devel@superboring.dev>
2021-10-14 20:58:58 +0200

    pkg: kms: fix test

--- pkg/kms/config_test.go
+++ pkg/kms/config_test.go
@@ -16,11 +16,12 @@ package kms
 
 import (
 	"fmt"
+	"os"
+	"testing"
+
 	"github.com/google/go-cmp/cmp"
 	"github.com/greenpau/caddy-authorize/internal/tests"
 	"github.com/greenpau/caddy-authorize/pkg/errors"
-	"os"
-	"testing"
 )
 
 func TestValidateCryptoKeyConfig(t *testing.T) {
@@ -571,7 +572,7 @@ func TestParseCryptoKeyConfigs(t *testing.T) {
 			shouldErr: true,
 			err: errors.ErrCryptoKeyConfigEntryInvalid.WithArgs(
 				`crypto key "foo`,
-				`record on line 1; parse error on line 2, column 0: extraneous or missing " in quoted-field`,
+				`parse error on line 1, column 16: extraneous or missing " in quoted-field`,
 			),
 		},
 		{
